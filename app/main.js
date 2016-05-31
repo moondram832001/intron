37db657d0eac86e526d703d8cafa157debac98c6
moondram832001 <moondram832001@gmail.com>
2016-05-31 23:28:41 +0000

    code cleanup

--- app/main.js
+++ app/main.js
@@ -2,9 +2,6 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import Intron from './containers/intron'
 import BB from './components/BB'
-//import Hexy from './components/Hexy'
-
-
 import IntronDemo from './components/IntronDemo'
 import Polaron from './components/Polaron'
 import Polarons from './components/Polarons'
@@ -14,18 +11,6 @@ import ReactKonva from './components/ReactKonva'
 
 let {Stage, Layer, Rect, Star, Circle,RegularPolygon,Path} = ReactKonva;
 
-
-
-//  <Stage height={1100} width={1300}>
-//         <Layer>
-//         {rr.map(function(i){
-//             let ff = parseInt((i-1) / 4) + 1 ;
-//             let gg = (i % 4) + 1;
-//             return <Polaron key={i} xPos={ff  * 120} yPos={gg * 130}/>;   
-//         })}
-//         </Layer>    
-//     </Stage>
-    
 ReactDOM.render(
     <IntronDemo></IntronDemo>
 ,
